0421550a642ce59062754bdbe0ac8269dd3d8ce6
khalilar42 <khalilarchkak@gmail.com>
2024-02-10 20:50:49 +0100

    Add a function to play a round of rock paper scissors game

--- script.js
+++ script.js
@@ -3,3 +3,27 @@ function getComputerChoice() {
     let randomIndex = Math.floor(Math.random() * 3);
     return gameChoices[randomIndex];
 }
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return "Draw ¯\\(◉‿◉)/¯";
+    }
+
+    let playerWon = false;
+    switch (playerSelection.toLowerCase()) {
+        case "rock":
+            computerSelection == "paper" ? (playerWon = false) : (playerWon = true);
+            break;
+        case "scissors":
+            computerSelection == "rock" ? (playerWon = false) : (playerWon = true);
+            break;
+        case "paper":
+            computerSelection == "scissors" ? (playerWon = false) : (playerWon = true);
+            break;
+    }
+
+    let playerLossMessage = "You lost (╯°□°)╯︵ ┻━┻ " + computerSelection + " beats " + playerSelection;
+    let playerWinMessage = "You won (ง ͡ʘ ͜ʖ ͡ʘ)ง " + playerSelection + " beats " + computerSelection;
+
+    return playerWon ? playerWinMessage : playerLossMessage;
+}
